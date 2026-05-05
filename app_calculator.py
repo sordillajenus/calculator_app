87e8d02337924caa4230923a3ad8fda70010c78f
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-05-05 22:48:50 +0800

    i set up a method first that makes the program running in loop by setting it to true, then methods that returns an operation based if they are addition, subtraction, multiplication or addition.

---
+++ app_calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__ (self):
+        self.running = True
+
+    def addition(self, a, b):
+        return a + b
+    
+    def subtraction(self, a, b):
+        return a - b
+    
+    def multiplication(self, a, b):
+        return a * b
+    
+    def division(self, a, b):
+        return a / b
+
