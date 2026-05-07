a9ce2975b68ed8f30eebdef015b609d6213121a5
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-05-07 12:46:23 +0800

    the initial block of code for asking operations

--- scientific.py
+++ scientific.py
@@ -46,7 +46,7 @@ class ScientificSymbolic:
         return sympy.integrate(expression, differential_dx)
     
     def ask_operation(self):
-        ops = [
+        operations = [
         "Sine", "Cosine", "Tangent", "Cosecant", "Secant", "Cotangent",
         "Logarithmic", "Hypotenuse", "Power", "Factorial", 
         "Squareroot", "Differentiation", "Integration"
@@ -55,7 +55,18 @@ class ScientificSymbolic:
         print("Scientific and Symbolic Calculator\n")
         print("Choose an operation\n")
 
-        for i 
+        for i, name in enumerate(operations, 1):
+            print(f"{i:2}. {name}")
+
+        try:
+            choice = input("Please choose an operation (1-13): ").strip()
+
+            if not choice.isdigit() or not str(1 <= int(choice) <= len(choice)):
+                raise ValueError("Plase Enter a Valid Choice.")
+            return choice
+        except ValueError("Enter a valid choice"):
+            return self.ask_operation
+
     
 
             
